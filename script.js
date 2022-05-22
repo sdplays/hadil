5e124b5af95b07a8b24373f68463eeef907a10d0
sdblock <77467572+sdplays@users.noreply.github.com>
2022-05-22 03:44:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,17 +3,17 @@ var h1 = document.querySelector('h1');
 var div = document.querySelector('div');
 var Rb =  Math.floor(Math.random() * 100) +'%' ;
 
-main.style.width = '1600px';
-main.style.height = '650px';
+main.style.width = '1200px';
+main.style.height = '500px';
 main.style.left =  '70px';
 main.style.top = '60px';
 main.style.backgroundColor = "#"+
  Math.floor(Math.random()*16777215).toString(16);
 
 const ball = document.createElement('div');
-var b = {x:50,y:30,w:Math.floor(Math.random() * 800),h:Math.floor(Math.random() * 
+var b = {x:50,y:30,w:Math.floor(Math.random() * 1100),h:Math.floor(Math.random() * 
 
-400),dx:Math.floor(Math.random() * 40),dy:Math.floor(Math.random() * 40),ani:
+450),dx:Math.floor(Math.random() * 40),dy:Math.floor(Math.random() * 40),ani:
 
 {},move:false};
 ball.style.backgroundColor = "#"+
@@ -24,12 +24,13 @@ ball.style.height = b.h +'px';
 ball.style.left = b.x + 'px';
 ball.style.top = b.y +'px';
 ball.style.position = 'relative';
+
 h1.addEventListener('click',()=>{
   main.style.backgroundColor = "#"+
  Math.floor(Math.random()*16777215).toString(16);
-   b = {x:50,y:30,w:Math.floor(Math.random() * 800),h:Math.floor(Math.random() * 
+   b = {x:50,y:30,w:Math.floor(Math.random() * 1100),h:Math.floor(Math.random() * 
 
-40),dx:Math.floor(Math.random() * 40),dy:Math.floor(Math.random() * 40),ani:
+450),dx:Math.floor(Math.random() * 40),dy:Math.floor(Math.random() * 40),ani:
 
 {},move:false};
 ball.style.backgroundColor = "#"+
@@ -54,13 +55,13 @@ div.addEventListener('click',()=>{
   
 })
 function mover(){
-  if(b.x>900-b.w || b.x < 0){
+  if(b.x>1200-b.w || b.x < 0){
     b.dx *= -1;
     
     ball.style.backgroundColor = "#"+
  Math.floor(Math.random()*16777215).toString(16);
   }
-  if(b.y>450-b.h || b.y < 0){
+  if(b.y>500-b.h || b.y < 0){
     b.dy *= -1;
     
     
