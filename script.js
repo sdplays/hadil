a4efadd71fa7e9d4a4d19e95a09c672e0b7cadc7
sdblock <77467572+sdplays@users.noreply.github.com>
2022-05-22 03:33:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ var h1 = document.querySelector('h1');
 var div = document.querySelector('div');
 var Rb =  Math.floor(Math.random() * 100) +'%' ;
 
-main.style.width = '1200px';
-main.style.height = '450px';
+main.style.width = '1600px';
+main.style.height = '650px';
 main.style.left =  '70px';
 main.style.top = '60px';
 main.style.backgroundColor = "#"+
