962c35ceca19e5bae5bfba4f62e2f63ec3469e55
sdblock <77467572+sdplays@users.noreply.github.com>
2022-05-31 00:15:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,72 +1,238 @@
-var main = document.querySelector(".main");
+var i =Math.floor(Math.random() * 
+11);
+var Rb =  Math.floor(Math.random() * 60) +'%' ;
+   
+var list = [
+  
+
+'data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAkGBxAOEBAPDw0PDxAREhAREBAQEB
+
+AQEA8QFRUaFhYRExUYHSggGRolGxMVIjEhJyorLi4uFx8zODMsNygtLisBCgoKBQUFDgUFDisZExkrKysrKysr
+
+KysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrK//AABEIAOEA4QMBIgACEQEDEQH/xA
+
+AcAAEAAgIDAQAAAAAAAAAAAAAAAQIHCAMFBgT/xAA9EAACAQMBBQQHBQcEAwAAAAAAAQIDBBEFBgchMUESUWGx
+
+EyJCUnGBkRQjMqHBM2JjcpLC0VOCoqMkNEP/xAAUAQEAAAAAAAAAAAAAAAAAAAAA/8QAFBEBAAAAAAAAAAAAAA
+
+AAAAAAAP/aAAwDAQACEQMRAD8AwaAAAAAAAACUizpS92X0YFAAAAAAAAAAAAAAAAAXVKT9mX0ZVoCAAAAAAAAA
+
+AAAAAvSpSnJRhGUpN4UYpuTfckuZ9+gaHcahXjb21NznL5RhHrKT6JGxuwe7y10qMZdlV7pr168lyfVU17K/MD
+
+Fmy2528ulGpdzVpTfHstdqq18OS+ZlDRN1GlW2G7Z3M17VaTks
+
++EVwPe06HfxOeKwB1NnoNvRWKVpb01+7Spx8kfZ9j6diGO7CPtAHn9R2Ys7hNV7G3qJ9XShn6pZPA7R7l7GunK
+
+0nO0qdI5c6T8MPijLxWdNPmgNP9q9jb3Sp4uaT9G36taHrU5fPo/BnnjdHU9Np16cqVanGrSmmpRmk00+8133n
+
+7s56a5XVopVLNv1o852zfSXfDx6de8DGwAAAAAd1szsvd6nU9Ha0XPD9ab4U4fzSPQ7td3dXV5+mq9qlZQeJVO
+
+UqzXOnT/WXT4myGiaNRtKUaFtSjRpRWEorn4t9X4gYy2a3KWtJRnfVZXM
+
++bpwbhST7u9mQtM2TsbZJULC3p46+ii5f1Syz0EKaRcD4VZJLChBLuSWD5rrRKFVNVLWhUT96lTl5o7chgY91v
+
+dbpVzn/AMT7PJ+3Qbhx
+
++HIxltTuYurdSqWVRXUFx9G8Qq48OjNjWcNSgny4AaVXNvOlOVOpCVOcXiUZJqSfimcRtTttsLa6rBqtD0ddL7
+
+u4gsTi
+
++5+9HwZrftVs1c6XXdC4hjm6dRfgqw96L/ToB0wAAAAActrbzrThSpxcpzlGEIrnKUnhI4jLO4fZpVa1TUascx
+
+o/d0MrnVa9afyi8f7n3AZM3d7HU9Kto00lK4qJOvUxzl7qfuo9tTgo8jit4YWerOZAXRKKokC6JKolASAAB8V5
+
+axlGUZRUoSTjKLWU0+aa7j7Q0Bq1vX2GelV/TUIv7JWb7H8KfWm/DuPAm4m1GhUr
+
++3q2lZZhUi0pdYS9ma8UzUnXNKq2NxWta0cVKM3CXc8cpLwaw18QPhPX7ttjJ6vc9mWY21LEq8/DpBPvZ5eytK
+
+lepCjSg51KkowhFc5Sk8JG2Ow2zENLtKVrDDmkpVqiX7Sq/wAUvh0XggO50vT6dGnClSgqdKmlGMYrCSXQ7JIi
+
+KxwJAAAAQwyrAMqySrArUinwZ5TbbZajqltO3qpKay6NXHGnPo14d6PWM4a8MrxQGmmradUtK9W3rR7NSlJwkv
+
+FdV4NYfzPkM1b+tmk409Spx9aPZo3GOsX+zm/g/V
+
++a7jCoAAADard3pCs9PtaGMS7CnPxnP1peZrHodv6a6t6eM9utSi14OSz
+
++Rt9ZQ7OF7sUl8uAH2osURZAWRZMoWQFiyZREgXTJKJk5AsCMkgUrQyvEwhv/ANme3ClqVOPrQxSr46wf4JP4P
+
+h8/AzkdLtJpcLu3r200nGtTlH4Nrg/qBhXcFsyqlWpqVWOY0s06GV/9GvWmvgnj5sz/AG8MLPf5Hnti9Bjp9nb
+
+2iw3Tj9448pTfGUvmz0wAAjIEkNkZIbANkAhsA2VZLKsAVZLKsDodqtMjd2tzbSXCpTml4PGU/qakVqbhKUJLE
+
+otxa7mnhm5lyuKNTtu7RUNSvKaWEq0n/V639wHQgADvthI51KyT/wBaJtjbc38jUrYur2NQs5PpXp/m8fqba2z
+
+4sD6EWRQlAXRJVMlMCyZZMoTkC+SclMk5AtknJXIAtk4blcEzkIqLKaA47Vc2c
+
++TjorCRbIFskZIGQJyRkjJGQJbK5GSABDDZVgGQwyAOG56GsG9yGNXuvFwf/FGz9z0NXd61Xt6vd49mUY/SK/y
+
+B5EAAfRp9f0VWlU9ypCf9Mk/0NwNNq9uMJLlKEX9Umabm0m7TVPtOm2dXOZRgqc/5oeq/ID2QAAnJKZUkC2Scl
+
+ck5AtknJXIyBfIyVAFsk5KZJyBbJGSMkZAtkZKgCckEZGQGSMjJGQGSAQAAAHzXUuPwWTUna679Pf3dT3q9THw
+
+T7K8jaHarUVbWt1cN8KdKbXxSwkjUicm223lttt97YEAAAZk3A65/7FhN/wAekm
+
++fszS/4v5sw2dpszrM9Pu6F3DOaU05R9+D4Th84toDb
+
++jLKLnX6VewrQp1acu1TqxjOL701lHYAAAAJIAFsjJUkC2RkrkZAvkZKZJyBbIyVyRkC2RkrkZAnJGQQBJAAAA
+
+ACtSWE2WPlu6qWW2lGKbk3ySXFtgYr3764qNpSs4y9e4l2prupQefzlj6MwOei3gbQPUr+vcJv0afo6K7qUOC
+
++rzL/czzoAAAAABmzcbtX24S02tL1oZnbt9Y+1BfDmZnozyvFczTXTb6pbVadelLs1KclOD8V
+
++htLsVtNT1K1p3NN+t+GrDrCoucX5oD1ACeeIAAAAAAAAAAAAAAAAAAAAAAAAbwBSrPC8jFe
+
++jav7JbfY6U/v7levh8YUev15HutpdbpWNvVuq0sQprguspezFeLZqvtDrNW/uat1Wfr1JZxnKhHpBeCQHXAAA
+
+AAAAAHq93W189JulNtytqmI3FNe70nFe8v8o8oANyNPvIVYQqU5qdOolKE08pp8mj7jXvdBt39lkrC6n9xOX3M
+
+5PhSm/Z/lZn23q9M/Bgc4AAAAAAAAAAAAAAAAAAAAAfLdV0k25JRim5NvCSXNt9xyV6uOC
+
++ZhPfFt5ntabaT8LmpF/9SfmB5Xeptq9UuPRUZP7JQbVP
+
++LPk6r8l4fE8KAAAAAAAAAAAAAzbuj3hel7GnXs/vFwtq0n+0X+lN
+
++93Pry54zhImMmmmm00001waa6oDdCjUzwfPzOUxNur3hq9jGzu5pXMElTqN49Ol/d5mVaNXPB8/MDkAAAAAAA
+
+AAAAAAAAAOOtVxwXPyFWp2fiY43m7fw0ym6FCSneTXBc1RT9uXj3ID4d7G8FWUZWVpPN3NfezXFW8H/e/y59xg
+
+OUm222228tvi2+9l7ivKpOVSpJznNuUpSeXKT5tnGAAAAAAAAAAAAAAAABelVlCSlCTjKLTjJPDTXVMz3uu3jx
+
+vlGzvJKF3FYp1HwjcpeU/DqYBLU6jg1KLcZRaaaeGmuTTA3Qo1c8Hz8zkMQ7sd5UbpQs72ajcLCp1XwjW7k
+
++6XmZZpVs8Hz7wOUAAAAAAAAAADjq1ccFz8iKtXHBc/IxpvK3iw06Mre2kql5JYb5xoZ6y/e8AObeXvChpcHQo
+
+ONS9muC5xoJ
+
++3Px7omvN3dVK1SdWrOVSpNuU5yeXJvqyLq4nWnKpUnKc5tylKTy5N9WcQAAAAAAAAAAAAAAAAAAAAABMW0008
+
+NcU1zTMy7st6GexZajPjwjQupPn3U6v6S+veYZAG59GvnGXlPkznNeN3G8+Vp2LW
+
++lKpb8Iwq850fCXfHyM9WN7CpCM4TVSnNJxnF5TT6pgfaAnkAAA2AOGtW6L6nHc3Kim3JRjFNyk3hJLm2+iMH7
+
+yN6Tq9u006bUOMalwuDn3xp9y8QO53mbzlbduz0+alX4xq11xjQ74w75+PTywbVqSnJylJylJtylJttt9Wyree
+
+LIAAAAAAAAAAAAAAAAAAAAAAAAAAAAey2B2/uNJmoPNa0k/Xot8Yd86b6Pw5M8aANu9nteoXtGNe2qqpTlzXtR
+
+fuyXRndQknyNQ9mNprnTKqq21Rrl26b406i7pL9TYbYbbu21WC7EvRXCX3lCT4/GHvID20pJczqtZ1eja0pV7i
+
+pGlSgstyf5LvZ0+2W2dtpdPt159qq193Qi125/4Xia77XbXXWq1e3XniCf3dGLfo6a+HV
+
++IHebwt49bU3KhQ7VGzT4Q5TrY9qo
+
++7908GAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAOW2uJ0pxqUpypzi8xnBuMovvTRxADnvbyrcTdWtVnVqS/F
+
+OpJyk/mzgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAD/2Q==',
+  'https://icons-for-free.com/download-icon-fb+logo+social+social+media+social
+
++network+icon-1320191784115198611_0.svg',
+  'https://pic.clubic.com/v1/images/1736117/raw?
+
+fit=smartCrop&height=675&width=1200&hash=c9ecb0757414cb558b9cf5b06e458304f63426d9',
+  
+
+'https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Twemoji12_1f602.svg/1200px-
+
+Twemoji12_1f602.svg.png',
+  'https://cdn.pixabay.com/photo/2020/12/19/02/50/emoji-5843433_1280.png',
+  
+
+'https://wikiimg.tojsiabtv.com/wikipedia/commons/thumb/e/e6/Noto_Emoji_KitKat_263a.svg
+
+/1200px-Noto_Emoji_KitKat_263a.svg.png',
+  
+
+'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/2048px-
+
+Instagram_icon.png',
+  
+
+'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/YouTube_social_white_squirc
+
+le.svg/1200px-YouTube_social_white_squircle.svg.png',
+  'https://icones.pro/wp-content/uploads/2021/03/logo-icone-tiktok-simbolo.png',
+  'https://cdn3.f-cdn.com/contestentries/1528078/19433701/5d19c2cd44ae0_thumb900.jpg',
+'https://miro.medium.com/max/1200/1*cLQUX8jM2bMdwMcV2yXWYA.jpeg'];
+var main = document.querySelector('.main');
 var h1 = document.querySelector('h1');
 var div = document.querySelector('div');
-var Rb =  Math.floor(Math.random() * 100) +'%' ;
-
-main.style.width = '1200px';
+var ball = new Image();
+ball.src = list[i]
+document.getElementById('body').appendChild(ball);
+main.style.width = '1100px';
 main.style.height = '500px';
-main.style.left =  '70px';
+main.style.left =  '50px';
 main.style.top = '60px';
-main.style.backgroundColor = "#"+
- Math.floor(Math.random()*16777215).toString(16);
+  main.style.backgroundColor = "black";
+   b = {x:Math.floor(Math.random() * 300),y:Math.floor(Math.random() * 
 
-const ball = document.createElement('div');
-var b = {x:50,y:30,w:Math.floor(Math.random() * 1100),h:Math.floor(Math.random() * 
-
-450),dx:Math.floor(Math.random() * 40),dy:Math.floor(Math.random() * 40),ani:
+100),w:Math.floor(Math.random() * 300)+40,h:Math.floor(Math.random() * 
+100)+40,dx:Math.floor(Math.random() * 30),dy:Math.floor(Math.random() * 30),ani:
 
 {},move:false};
-ball.style.backgroundColor = "#"+
- Math.floor(Math.random()*16777215).toString(16);
-ball.style.borderRadius = Rb;
-ball.style.width = b.w +'px';
-ball.style.height = b.h +'px';
 ball.style.left = b.x + 'px';
 ball.style.top = b.y +'px';
+ball.style.width = b.w +'px';
+ball.style.height = b.h +'px'; 
+ball.style.borderRadius = Rb;
 ball.style.position = 'relative';
 
-h1.addEventListener('click',()=>{
-  main.style.backgroundColor = "#"+
- Math.floor(Math.random()*16777215).toString(16);
-   b = {x:50,y:30,w:Math.floor(Math.random() * 1100),h:Math.floor(Math.random() * 
 
-450),dx:Math.floor(Math.random() * 40),dy:Math.floor(Math.random() * 40),ani:
+
+h1.addEventListener('click',()=>{
+  
+  main.style.backgroundColor = "black";
+  Rb =  Math.floor(Math.random() * 60) +'%' ;
+   
+   b = {x:Math.floor(Math.random() * 100),y:Math.floor(Math.random() * 
+
+100),w:Math.floor(Math.random() * 300)+40,h:Math.floor(Math.random() * 
+300)+40,dx:Math.floor(Math.random() * 30),dy:Math.floor(Math.random() * 30),ani:
 
 {},move:false};
-ball.style.backgroundColor = "#"+
- Math.floor(Math.random()*16777215).toString(16);
-ball.style.borderRadius = Rb;
-ball.style.width = b.w +'px';
-ball.style.height = b.h +'px';
+  i =Math.floor(Math.random() *
+11);
+ball.src = list[i];
 ball.style.left = b.x + 'px';
 ball.style.top = b.y +'px';
-  Rb =  Math.floor(Math.random() * 100) +'%' ;
+ball.style.width = b.w +'px';
+ball.style.height = b.h +'px';  
+ ball.style.borderRadius = Rb;
    
 })
 div.addEventListener('click',()=>{
+  
   if(!b.move){
    b.ani = requestAnimationFrame(mover);
    b.move = true
   }else{
     cancelAnimationFrame(b.ani)
     b.move = false
-  }
+  }})
+ball.addEventListener('click',()=>{
+  var l = Math.floor(Math.random() * 11)
+  var img = new Image();
+img.src = list[l]
+document.getElementById('body').appendChild(img);
+img.style.borderRadius =  Math.floor(Math.random() * 100) +'%' ;
   
-  
+img.style.left = Math.floor(Math.random() * 100)+ 'px';
+img.style.top = Math.floor(Math.random() * 100)+'px';
+img.style.width = Math.floor(Math.random() * 200)+10 +'px';
+img.style.height = Math.floor(Math.random() * 
+100)+10 +'px'; 
+img.style.position = 'relative';
 })
+  
+  
+
 function mover(){
-  if(b.x>1200-b.w || b.x < 0){
+  if(b.x>1100-b.w || b.x < 0){
     b.dx *= -1;
-    
-    ball.style.backgroundColor = "#"+
- Math.floor(Math.random()*16777215).toString(16);
+
   }
   if(b.y>500-b.h || b.y < 0){
     b.dy *= -1;
     
-    
-   main.style.backgroundColor = "#"+
- Math.floor(Math.random()*16777215).toString(16); 
   }
   b.x += b.dx;
   b.y += b.dy;
