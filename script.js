1829cdc088404d4f824eae4c274d8d7373b178a5
sdblock <77467572+sdplays@users.noreply.github.com>
2022-05-31 00:26:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,10 @@ var i =Math.floor(Math.random() *
 var Rb =  Math.floor(Math.random() * 60) +'%' ;
    
 var list = [
-  
-
-'data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAkGBxAOEBAPDw0PDxAREhAREBAQEB
-
+ 
+"data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAkGBxAOEBAPDw0PDxAREhAREBAQEB
 AQEA8QFRUaFhYRExUYHSggGRolGxMVIjEhJyorLi4uFx8zODMsNygtLisBCgoKBQUFDgUFDisZExkrKysrKysr
-
 KysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrK//AABEIAOEA4QMBIgACEQEDEQH/xA
-
 AcAAEAAgIDAQAAAAAAAAAAAAAAAQIHCAMFBgT/xAA9EAACAQMBBQQHBQcEAwAAAAAAAQIDBBEFBgchMUESUWGx
 
 EyJCUnGBkRQjMqHBM2JjcpLC0VOCoqMkNEP/xAAUAQEAAAAAAAAAAAAAAAAAAAAA/8QAFBEBAAAAAAAAAAAAAA
@@ -62,92 +58,50 @@ h8/AzkdLtJpcLu3r200nGtTlH4Nrg/qBhXcFsyqlWpqVWOY0s06GV/9GvWmvgnj5sz/AG8MLPf5Hnti9
 otxa7mnhm5lyuKNTtu7RUNSvKaWEq0n/V639wHQgADvthI51KyT/wBaJtjbc38jUrYur2NQs5PpXp/m8fqba2z
 
 4sD6EWRQlAXRJVMlMCyZZMoTkC+SclMk5AtknJXIAtk4blcEzkIqLKaA47Vc2c
-
 +TjorCRbIFskZIGQJyRkjJGQJbK5GSABDDZVgGQwyAOG56GsG9yGNXuvFwf/FGz9z0NXd61Xt6vd49mUY/SK/y
-
 B5EAAfRp9f0VWlU9ypCf9Mk/0NwNNq9uMJLlKEX9Umabm0m7TVPtOm2dXOZRgqc/5oeq/ID2QAAnJKZUkC2Scl
-
 ck5AtknJXIyBfIyVAFsk5KZJyBbJGSMkZAtkZKgCckEZGQGSMjJGQGSAQAAAHzXUuPwWTUna679Pf3dT3q9THw
-
 T7K8jaHarUVbWt1cN8KdKbXxSwkjUicm223lttt97YEAAAZk3A65/7FhN/wAekm
-
 +fszS/4v5sw2dpszrM9Pu6F3DOaU05R9+D4Th84toDb
-
 +jLKLnX6VewrQp1acu1TqxjOL701lHYAAAAJIAFsjJUkC2RkrkZAvkZKZJyBbIyVyRkC2RkrkZAnJGQQBJAAAA
-
 ACtSWE2WPlu6qWW2lGKbk3ySXFtgYr3764qNpSs4y9e4l2prupQefzlj6MwOei3gbQPUr+vcJv0afo6K7qUOC
-
 +rzL/czzoAAAAABmzcbtX24S02tL1oZnbt9Y+1BfDmZnozyvFczTXTb6pbVadelLs1KclOD8V
-
 +htLsVtNT1K1p3NN+t+GrDrCoucX5oD1ACeeIAAAAAAAAAAAAAAAAAAAAAAAAbwBSrPC8jFe
-
 +jav7JbfY6U/v7levh8YUev15HutpdbpWNvVuq0sQprguspezFeLZqvtDrNW/uat1Wfr1JZxnKhHpBeCQHXAAA
-
 AAAAAHq93W189JulNtytqmI3FNe70nFe8v8o8oANyNPvIVYQqU5qdOolKE08pp8mj7jXvdBt39lkrC6n9xOX3M
-
 5PhSm/Z/lZn23q9M/Bgc4AAAAAAAAAAAAAAAAAAAAAfLdV0k25JRim5NvCSXNt9xyV6uOC
-
 +ZhPfFt5ntabaT8LmpF/9SfmB5Xeptq9UuPRUZP7JQbVP
-
 +LPk6r8l4fE8KAAAAAAAAAAAAAzbuj3hel7GnXs/vFwtq0n+0X+lN
-
 +93Pry54zhImMmmmm00001waa6oDdCjUzwfPzOUxNur3hq9jGzu5pXMElTqN49Ol/d5mVaNXPB8/MDkAAAAAAA
-
 AAAAAAAAAOOtVxwXPyFWp2fiY43m7fw0ym6FCSneTXBc1RT9uXj3ID4d7G8FWUZWVpPN3NfezXFW8H/e/y59xg
-
 OUm222228tvi2+9l7ivKpOVSpJznNuUpSeXKT5tnGAAAAAAAAAAAAAAAABelVlCSlCTjKLTjJPDTXVMz3uu3jx
-
 vlGzvJKF3FYp1HwjcpeU/DqYBLU6jg1KLcZRaaaeGmuTTA3Qo1c8Hz8zkMQ7sd5UbpQs72ajcLCp1XwjW7k
-
 +6XmZZpVs8Hz7wOUAAAAAAAAAADjq1ccFz8iKtXHBc/IxpvK3iw06Mre2kql5JYb5xoZ6y/e8AObeXvChpcHQo
-
-ONS9muC5xoJ
-
-+3Px7omvN3dVK1SdWrOVSpNuU5yeXJvqyLq4nWnKpUnKc5tylKTy5N9WcQAAAAAAAAAAAAAAAAAAAAABMW0008
-
+ONS9muC5xoJ+3Px7omvN3dVK1SdWrOVSpNuU5yeXJvqyLq4nWnKpUnKc5tylKTy5N9WcQAAAAAAAAAAAAAAAAAAAAABMW0008
 NcU1zTMy7st6GexZajPjwjQupPn3U6v6S+veYZAG59GvnGXlPkznNeN3G8+Vp2LW
-
 +lKpb8Iwq850fCXfHyM9WN7CpCM4TVSnNJxnF5TT6pgfaAnkAAA2AOGtW6L6nHc3Kim3JRjFNyk3hJLm2+iMH7
-
 yN6Tq9u006bUOMalwuDn3xp9y8QO53mbzlbduz0+alX4xq11xjQ74w75+PTywbVqSnJylJylJtylJttt9Wyree
-
 LIAAAAAAAAAAAAAAAAAAAAAAAAAAAAey2B2/uNJmoPNa0k/Xot8Yd86b6Pw5M8aANu9nteoXtGNe2qqpTlzXtR
-
 fuyXRndQknyNQ9mNprnTKqq21Rrl26b406i7pL9TYbYbbu21WC7EvRXCX3lCT4/GHvID20pJczqtZ1eja0pV7i
-
 pGlSgstyf5LvZ0+2W2dtpdPt159qq193Qi125/4Xia77XbXXWq1e3XniCf3dGLfo6a+HV
-
-+IHebwt49bU3KhQ7VGzT4Q5TrY9qo
-
-+7908GAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAOW2uJ0pxqUpypzi8xnBuMovvTRxADnvbyrcTdWtVnVqS/F
-
-OpJyk/mzgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAD/2Q==',
++IHebwt49bU3KhQ7VGzT4Q5TrY9qo+7908GAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAOW2uJ0pxqUpypzi8xnBuMovvTRxADnvbyrcTdWtVnVqS/F
+OpJyk/mzgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAD/2Q==",
   'https://icons-for-free.com/download-icon-fb+logo+social+social+media+social
-
 +network+icon-1320191784115198611_0.svg',
   'https://pic.clubic.com/v1/images/1736117/raw?
-
 fit=smartCrop&height=675&width=1200&hash=c9ecb0757414cb558b9cf5b06e458304f63426d9',
   
 
 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Twemoji12_1f602.svg/1200px-
-
 Twemoji12_1f602.svg.png',
   'https://cdn.pixabay.com/photo/2020/12/19/02/50/emoji-5843433_1280.png',
   
 
 'https://wikiimg.tojsiabtv.com/wikipedia/commons/thumb/e/e6/Noto_Emoji_KitKat_263a.svg
-
 /1200px-Noto_Emoji_KitKat_263a.svg.png',
-  
-
 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/2048px-
-
 Instagram_icon.png',
-  
-
 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/YouTube_social_white_squirc
-
 le.svg/1200px-YouTube_social_white_squircle.svg.png',
   'https://icones.pro/wp-content/uploads/2021/03/logo-icone-tiktok-simbolo.png',
   'https://cdn3.f-cdn.com/contestentries/1528078/19433701/5d19c2cd44ae0_thumb900.jpg',
